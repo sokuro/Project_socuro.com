c59b6acc0f19645b3212541e0113ae20fdc87f92
ugorcak <ugorcak@gmx.ch>
2016-04-03 19:46:43 +0200

    changed image structure

---
+++ assets/js/ChangingFormTwo.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Kajo on 4/3/2016.
+ */
+if (document.images) {
+    var imageOne = new Image(); //preload
+    imageOne.src = "assets/resources/pics/geometry/Circle_001.png";
+    var imageTwo = new Image();
+    imageTwo.src = "assets/resources/pics/geometry/Circle_002.png";
+    var imageThree = new Image();
+    imageThree.src = "assets/resources/pics/geometry/Circle_003.png";
+    var imageFour = new Image();
+    imageFour.src = "assets/resources/pics/geometry/Circle_004.png";
+    var imageFive = new Image();
+    imageFive.src = "assets/resources/pics/geometry/Circle_005.png";
+    var imageSix = new Image();
+    imageSix.src = "assets/resources/pics/geometry/Circle_006.png";
+    var imageSeven = new Image();
+    imageSeven.src = "assets/resources/pics/geometry/Circle_007.png";
+    var imageEight = new Iamge();
+    imageEight.src = "assets/resources/pics/geometry/Circle_008.png";
+    var imageNine = new Image();
+    imageNine.src = "assets/resources/pics/geometry/Circle_009.png";
+}

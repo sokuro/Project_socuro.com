5bb1e278e24a67c8a0c792248b05a2190da2290a
ugorcak <ugorcak@gmx.ch>
2016-04-03 20:36:35 +0200

    changed image structure

--- assets/js/ChangingForm.js
+++ assets/js/ChangingForm.js
@@ -1,43 +1,44 @@
 /**
  * Created by Kajo on 4/3/2016.
  */
-var getImage = function (images) {
+// var getImage = function (images) {
+var images = 'imageOne';
     switch (images) {
-        case "imageOne":
+        case 'imageOne':
             var imageOne = new Image(); //preload
             imageOne.src = "assets/resources/pics/geometry/Circle_001.png";
             break;
-        case "imageTwo":
+        case 'imageTwo':
             var imageTwo = new Image();
             imageTwo.src = "assets/resources/pics/geometry/Circle_002.png";
             break;
-        case "imageThree":
+        case 'imageThree':
             var imageThree = new Image();
             imageThree.src = "assets/resources/pics/geometry/Circle_003.png";
             break;
-        case "imageFour":
+        case 'imageFour':
             var imageFour = new Image();
             imageFour.src = "assets/resources/pics/geometry/Circle_004.png";
             break;
-        case "imageFive":
+        case 'imageFive':
             var imageFive = new Image();
             imageFive.src = "assets/resources/pics/geometry/Circle_005.png";
             break;
-        case "imageSix":
+        case 'imageSix':
             var imageSix = new Image();
             imageSix.src = "assets/resources/pics/geometry/Circle_006.png";
             break;
-        case "imageSeven":
+        case 'imageSeven':
             var imageSeven = new Image();
             imageSeven.src = "assets/resources/pics/geometry/Circle_007.png";
             break;
-        case "imageEight":
+        case 'imageEight':
             var imageEight = new Iamge();
             imageEight.src = "assets/resources/pics/geometry/Circle_008.png";
             break;
-        case "imageNine":
+        case 'imageNine':
             var imageNine = new Image();
             imageNine.src = "assets/resources/pics/geometry/Circle_009.png";
             break;
     }
-}
+// }
